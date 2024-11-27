585e260baa79f2513455269d2c0675bc055d761c
Anton Kostiuk <anton_kostyuk@mail.ru>
2024-11-27 13:55:43 +0800

    update bin/

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import readFile from '../src/utils.js';
+import parse from '../src/parser.js';
+
 const program = new Command();
 
 program
@@ -9,7 +12,13 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
   .argument('<filepath1>')
-  .argument('<filepath2>');
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    const result1 = readFile(filepath1);
+    const result2 = readFile(filepath2);
+    console.log(parse(result1));
+    console.log(parse(result2));
+  });
 
 program.parse();
 
