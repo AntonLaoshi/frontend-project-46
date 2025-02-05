7e10e716d9bbb9bea2a19148a661eac214d4c179
Anton Kostiuk <anton_kostyuk@mail.ru>
2025-02-05 13:00:40 +0800

    add bin

---
+++ bin/gendiff.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+import genDiff from '../src/index.js'
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'otput format (default: "stylish")')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2, option) => {
+    const result = genDiff(filepath1, filepath2, option.format);
+    console.log(result);
+  });
+
+program.parse();
