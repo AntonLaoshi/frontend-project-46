9c561c0bf3e06692196e59194cadd952383c1d11
Anton Kostiuk <anton_kostyuk@mail.ru>
2025-02-05 16:06:21 +0800

    update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,7 +5,6 @@ import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
